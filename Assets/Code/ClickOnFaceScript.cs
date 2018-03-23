ddb543671b65f21df54fe08b30171b593c06a987
virtue244 <virtue2021@gmail.com>
2018-03-23 18:54:09 -0400

    ssets folder

---
+++ Assets/Code/ClickOnFaceScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOnFaceScript : MonoBehaviour {
+
+    // Public fields are visible and their values can be changed directly in the editor
+    // rrepresents the position displacement needed to compure the position of the new instance
+    public Vector3 delta;
+
+    // This function is triggered when the mouse cursor is over the GameObject on which this scrpit runs
+    void OnMouseOver()
+    {
+        // If the left mouse button is pressed
+        if (Input.GetMouseButtonDown(1))
+        {
+            // Display a message in the Console tab
+            Debug.Log("Left click!");
+            // Destroy the parent of the face we clicked
+            Destroy(this.transform.parent.gameObject);
+        }
+
+        // If the left mouse button is pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Display a message in the Console tab
+            Debug.Log("Right click!");
+            WorldGenerator.CloneAndPlace(this.transform.parent.transform.position + delta, // N = C + delta
+                this.transform.parent.gameObject);
+        }
+    }
+}
---
+++ Assets/Code/WorldGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour {
+
+    // Drag and drop here the voxel from the scene
+    // Used to create new instances
+    public GameObject Voxel;
+
+    //Specify the dimensions of the world
+    public float SizeX;
+    public float SizeZ;
+    public float SizeY;
+
+	// Use this for initialization
+	void Start () {
+        //Start the world generation coroutine
+        // StartCoroutine function always returns immediately, however you can yield the result
+        StartCoroutine(SimpleGenerator());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void CloneAndPlace(Vector3 newPosition,
+                                    GameObject originalGameObject)
+    {
+        // Clone
+        GameObject clone = (GameObject)Instantiate(originalGameObject, newPosition, Quaternion.identity);
+
+        //Place
+        clone.transform.position = newPosition;
+        //Rename
+        clone.name = "Cube@" + clone.transform.position;
+    }
+
+    IEnumerator SimpleGenerator()
+    {
+        uint numberOfInstances = 0;
+        uint instancesPerFrame = 50;
+
+        for(int x = 1; x <= SizeX; x++)
+        {
+            for(int z = 1; z <= SizeZ; z++)
+            {
+                //compute a random height
+                float height = Random.Range(0, SizeY);
+                for (int y = 0; y <= height; y++)
+                {
+                    Vector3 newPosition = new Vector3(x, y, z);
+                    CloneAndPlace(newPosition, Voxel);
+                    numberOfInstances++;
+
+                    if (numberOfInstances == instancesPerFrame)
+                    {
+                        numberOfInstances = 0;
+                        yield return new WaitForEndOfFrame();
+                    }
+                }
+            }
+        }
+    }
+}
